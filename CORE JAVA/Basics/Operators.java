0e3082359009dd10aa3931f33e36e56d5f960aad
Venkatasandeepadapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-11 15:24:19 +0530

    Create Operators.java

---
+++ CORE JAVA/Basics/Operators.java
@@ -0,0 +1,22 @@
+class Operators
+  {
+    public static void main(String args[])
+    {
+      int a = 22;
+      int b = 5;
+      System.out.println("Arithmetic Operator");
+      System.out.println(a+b);
+      System.out.println(a-b);
+      System.out.println(a*b);
+      System.out.println(a/b);
+      System.out.println(a % b);      
+
+      System.out.println("Relational Operator");
+      System.out.println(a += b);
+      System.out.println(a -= b);
+      System.out.println(a *= b);
+      System.out.println(a /= b);
+      System.out.println(a %= b);      
+
+    }
+  }
