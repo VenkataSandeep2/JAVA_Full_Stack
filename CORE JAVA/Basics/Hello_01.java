29de4552ec8d1e63bdf2b8764a24fa5946938e45
Venkatasandeepadapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-14 21:49:26 +0530

    Update Hello_01.java

--- CORE JAVA/Basics/Hello_01.java
+++ CORE JAVA/Basics/Hello_01.java
@@ -2,7 +2,7 @@ class Hello_01
   {
     public static void main(String args[])
     {
-      String s ="Hello";
+      String s ="Hello java";
       int i = 515;
       System.out.println(s +"  "+i);
     }
