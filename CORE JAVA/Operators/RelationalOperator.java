f668a913054a7c07ec610f76cc69333365b808d7
Venkatasandeepadapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-16 11:23:39 +0530

    Add RelationalOperator class with relational operations
    Implement RelationalOperator class to demonstrate relational operations in Java.

---
+++ CORE JAVA/Operators/RelationalOperator.java
@@ -0,0 +1,45 @@
+class RelationalOperator
+  {
+    public static void main(String args[])
+    {
+      int a = 10;
+      int b = 20;
+      int c = 10;
+      System.out.println(a==b);
+      System.out.println(a==c);
+      System.out.println();
+      boolean check = (a==b);
+      System.out.println(check);
+      System.out.println(a < b);
+      System.out.println(a > b);
+      System.out.println(b > c);
+      System.out.println(a >= b);
+      System.out.println(a >= c);
+      System.out.println(a <= b);
+      System.out.println(a != b);
+      System.out.println(a != c);
+      
+    }
+  }
+
+
+/*
+OUTPUT
+
+false
+true
+
+false
+true
+false
+true
+false
+true
+true
+true
+false
+
+=== Code Execution Successful ===
+
+
+*/
