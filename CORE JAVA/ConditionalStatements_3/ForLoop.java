abb8c26fcd476715aa908472c67e6686b16724f3
Venkatasandeepadapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-21 07:59:25 +0530

    Add ForLoop class to demonstrate for loop usage
    Implement a For Loop to print numbers from 0 to 9.

---
+++ CORE JAVA/ConditionalStatements_3/ForLoop.java
@@ -0,0 +1,27 @@
+class ForLoop
+  {
+    public static void main(String args[])
+    {
+      for(int i = 0; i< 10; i++)
+        {
+          System.out.println(i + " times");
+        }
+    }
+  }
+
+/*
+      output
+0 times
+1 times
+2 times
+3 times
+4 times
+5 times
+6 times
+7 times
+8 times
+9 times
+
+=== Code Execution Successful ===
+
+  */
